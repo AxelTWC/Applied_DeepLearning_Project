f13946f487e541b8184b982d15563404c17bdfaa
“buruei” <buruei@hotmail.com>
2025-12-01 11:19:32 -0500

    feat: update FaissLocalRetrieve to work with FaissLocalVectorStore

--- rag_pipeline/retriever.py
+++ rag_pipeline/retriever.py
@@ -1,12 +1,15 @@
+from curses import raw
 from typing import List, Dict, Optional
-from embeddings import EmbeddingModel
-from vectorstore import InMemoryVectorStore
-from chunking import FixedSizeChunk
-from vectorstore import FassisLocalVectorStore
+from sentence_transformers import SentenceTransformer
+from .embeddings import EmbeddingModel
+from .vectorstore import InMemoryVectorStore
+from .chunking import FixedSizeChunk
+from .vectorstore import FaissLocalVectorStore
 from pyserini.search.faiss import FaissSearcher
+from pyserini.search.lucene import LuceneSearcher
 from pyserini.encode import DprQueryEncoder
 from datasets import load_dataset
-from typing import List, Dict, Optional
+from typing import List, Dict, Optional, Tuple
 import numpy as np
 import json
 from tqdm import tqdm
@@ -18,6 +21,8 @@ class Retriever:
 
     Documents are dicts containing at least 'id' and 'text'. Metadata may be provided.
     """
+    
+    name: str = "BaseRetriever"
 
     def __init__(self, embedding_model: Optional[EmbeddingModel] = None, chunk_size: int = 500, overlap: int = 50):
         self.embedding_model = embedding_model or EmbeddingModel()
@@ -57,24 +62,21 @@ class Retriever:
     def retrieve(self, query: str, top_k: int = 5):
         q_emb = self.embedding_model.embed_texts([query])[0]
         return self.store.search(q_emb, top_k=top_k)
-
-class FassisLocalRetriever(Retriever):
-    """
-    Use fassis vector store from Langchain to build retriever with FAISS indexing on local disk
-    """
-    def __init__(self, embedding_model: str = "all-MiniLM-L6-v2"):
-        self.store = FassisLocalVectorStore(embedding_model=embedding_model)
-        
-    def add_documents(self, documents: List[Dict]):
-        self.store.store(documents)
-        
-    def retrieve(self, query: str, top_k: int = 5):
-        return self.store.search(query, top_k=top_k)
+    
+    def retrieve_batch(self, questions: List[str], question_ids: List[int] = None, top_k: int = 5, threads: int = 1):
+        answers = []
+        for question in questions:
+            answers.append(self.retrieve(question, top_k=top_k))
+        return answers
+    
+    def get_document(self, doc_id: str) -> Optional[Dict]:
+        return self.store.get_document(doc_id)
     
 class DPRRetriever(Retriever):
     """
     Use DPR to build retriever with FAISS indexing on local disk
     """
+    name: str = "DPR"
     def __init__(self):
         if torch.cuda.is_available():
             self.device = "cuda"
@@ -89,9 +91,15 @@ class DPRRetriever(Retriever):
         pass
         
     def retrieve(self, query: str, top_k: int = 5):
-        return self.searcher.search(query, k=top_k)
+        raw_contexts = self.searcher.search(query, k=top_k)
+        results = []
+        scores = []
+        for context in raw_contexts:
+            results.append(json.loads(self.searcher.doc(context.docid).raw())["contents"])
+            scores.append(float(context.score))
+        return raw_contexts, results, scores
     
-    def retrieve_batch(self, questions: List[str], question_ids: List[int] = None, top_k: int = 5, threads: int = 1):
+    def retrieve_batch(self, questions: List[str], question_ids: List[int] = None, top_k: int = 5, threads: int = 1) -> Tuple[Dict[str, List], Dict[str, Tuple[List, List]]]:
         """
         Retrieve batch of questions
         Args:
@@ -100,135 +108,132 @@ class DPRRetriever(Retriever):
             top_k (int, optional): Number of top documents to retrieve for each question. Defaults to 5.
             threads (int, optional): Number of threads to use for retrieval. Defaults to 1.
         Returns: 
-            Dict[str, List[DenseSearchResult]]
-            returns a dictionary holding the search results, with the query ids as keys and the corresponding lists of search results as the values.
+            Dict[str, List[DenseSearchResult]] - a dictionary holding the raw search results, with the query ids as keys and the corresponding lists of search results as the values.
+            Dict[str, Tuple[List, List]] - a dictionary holding the corresponding list of retrieved text contexts and list of scores with question id as keys.
         """
         if question_ids is None:
             question_ids = list(range(len(questions)))
-        return self.searcher.batch_search(questions, question_ids, k=top_k, threads=threads)
+        batch_contexts = self.searcher.batch_search(questions, question_ids, k=top_k, threads=threads)
+        answers = {}
+        for question_id in question_ids:
+            question = questions[question_id]
+            results = []
+            scores = []
+            contexts = batch_contexts[question_id]
+            for context in contexts:
+                results.append(json.loads(self.searcher.doc(context.docid).raw())["contents"])
+                scores.append(float(context.score))
+            answers[question_id] = (results, scores)
+        return batch_contexts, answers
     
-    def preprocess_triviaqa(self, filepath: str, top_k: int = 10, preprocess_length: Optional[int] = None, batch_size: Optional[int]=None, threads : int = 1, split: str = "validation") -> List[Dict]:
-        dataset = load_dataset("trivia_qa", "rc", split=split).shuffle(seed=1508)
-        os.makedirs(filepath, exist_ok=True)
-        if preprocess_length is None:
-            preprocess_length = len(dataset)
-        if batch_size is None:
-            batch_size = preprocess_length
-        output_filepath = os.path.join(filepath, f"triviaqa_{split}_{preprocess_length}_DPR.json")
-        retrieved_docs = []
-        if os.path.exists(output_filepath):
-            with open(output_filepath, 'r', encoding='utf-8') as f:
-                retrieved_docs = json.load(f)
-            print(f"Loaded {len(retrieved_docs)} preprocessed documents from {output_filepath}")
+    def get_document(self, doc_id: str) -> Optional[Dict]:
+        return self.searcher.get_document(doc_id)
+    
+class BM25Retriever(Retriever):
+    """
+    Use BM25 to build retriever with FAISS indexing on local disk
+    """
+    
+    name: str = "BM25"
+    def __init__(self):
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        elif torch.backends.mps.is_available():
+            self.device = "mps"
+        else:
+            self.device = "cpu"
+        self.searcher = LuceneSearcher.from_prebuilt_index('wikipedia-dpr-100w')
         
-        start_idx = len(retrieved_docs)
-        remaining_length = min(preprocess_length - start_idx, len(dataset) - start_idx)
+    def add_documents(self, documents: List[Dict]):
+        pass
         
-        if remaining_length <= 0:
-            print(f"Already processed {len(retrieved_docs)} documents, target is {preprocess_length}")
-            return retrieved_docs
+    def retrieve(self, query: str, top_k: int = 5):
+        contexts = self.searcher.search(query, k=top_k)
+        answers = [
+            json.loads(self.searcher.doc(context.docid).raw())["contents"] for context in contexts
+        ]
+        scores = [
+            float(context.score) for context in contexts
+        ]
+        return contexts, answers, scores
+    
+    def retrieve_batch(self, questions: List[str], question_ids: List[int] = None, top_k: int = 5, threads: int = 1) -> Tuple[Dict[str, List], Dict[str, Tuple[List, List]]]:
+        """
+        Retrieve batch of questions
+        Args:
+            questions (List[str]): List of questions to retrieve documents for.
+            question_ids (List[int], optional): List of question IDs corresponding to the questions. Defaults to None. if None, question IDs will be set to range(len(questions)).
+            top_k (int, optional): Number of top documents to retrieve for each question. Defaults to 5.
+            threads (int, optional): Number of threads to use for retrieval. Defaults to 1.
+        Returns: 
+            Dict[str, List] - a dictionary holding the raw search results, with the query ids as keys and the corresponding lists of search results as the values.
+            Dict[str, Tuple[List, List]] - a dictionary holding the corresponding list of retrieved text contexts and list of scores with question id as keys.
+        """
+        if question_ids is None:
+            assert False, "question_ids cannot be None for BM25Retriever" 
+        batch_contexts = self.searcher.batch_search(questions, question_ids, k=top_k, threads=threads)
+        answers = {}
+        for question, question_id in zip(questions, question_ids):
+            contexts = batch_contexts[question_id]
+            results = [json.loads(context.lucene_document.get('raw')).get("contents") for context in contexts]
+            scores = [float(context.score) for context in contexts]
+            answers[question_id] = (results, scores)
+        return batch_contexts, answers
+    
+class FaissLocalRetriever(Retriever):
+    name: str = "BgeM3Faiss"
+    def __init__(self, embedding_model: str = "BAAI/bge-m3", faiss_index_path: str = "../faiss_store"):
+        self.store = FaissLocalVectorStore(embedding_model=embedding_model, faiss_index_path=faiss_index_path)
+        if torch.cuda.is_available():
+            self.device = "cuda"
+        elif torch.backends.mps.is_available():
+            self.device = "mps"
+        else:
+            self.device = "cpu"
             
-        subset = dataset.select(range(start_idx, start_idx + remaining_length))
-        with tqdm(desc="Preprocessing TriviaQA", total=preprocess_length) as pbar:
-            pbar.update(len(retrieved_docs))
-            for i in range(0, remaining_length, batch_size):
-                batch_end = min(i + batch_size, remaining_length)
-                batch = subset.select(range(i, batch_end))
-                question_ids = [doc['question_id'] for doc in batch]
-                questions = [doc['question'] for doc in batch]
-                answers = [doc['answer']['aliases'] for doc in batch]
-                batch_contexts = self.searcher.batch_search(questions, question_ids, k=top_k, threads=threads)
-                context_list = []
-                context_score_list = []
-                for question_id, question, answer in zip(question_ids, questions, answers):
-                    retrieved_doc = {}
-                    retrieved_doc['question_id'] = question_id
-                    retrieved_doc['question'] = question
-                    retrieved_doc['answer'] = answer
-                    contexts = batch_contexts[question_id]
-                    context_score_list = []
-                    context_list = []
-                    for context in contexts:
-                        context_list.append(json.loads(self.searcher.doc(context.docid).raw())["contents"])
-                        context_score_list.append(float(context.score))
-                    retrieved_doc['contexts'] = context_list
-                    retrieved_doc['context_scores'] = context_score_list
-                    retrieved_docs.append(retrieved_doc)
-                pbar.update(len(batch))
-                with open(output_filepath, 'w', encoding='utf-8') as f:
-                    json.dump(retrieved_docs, f, ensure_ascii=False, indent=4)
-        return retrieved_docs
+        
+    def retrieve(self, query: str, top_k: int = 5):
+        context_raw = self.store.search(query, top_k=top_k)
+        answers = [res["text"] for res in context_raw]
+        scores = [res["score"] for res in context_raw]
+        return context_raw, answers, scores
     
-    def preprocess_nq(self, filepath: str, top_k: int = 10, preprocess_length: Optional[int] = None, threads : int = 1, batch_size: Optional[int]=None, split: str = "validation") -> List[Dict]:
-        dataset = load_dataset("google-research-datasets/natural_questions", "default", split=split).shuffle(seed=1508)
-        retrieved_docs = []
-        idx = 0
-        if preprocess_length is None:
-            preprocess_length = len(dataset)
-        if batch_size is None:
-            batch_size = preprocess_length
-        os.makedirs(filepath, exist_ok=True)
-        output_filepath = os.path.join(filepath, f"nq_{split}_{preprocess_length}_DPR.json")
-        if os.path.exists(output_filepath):
-            with open(output_filepath, 'r', encoding='utf-8') as f:
-                retrieved_docs = json.load(f)
-            print(f"Loaded {len(retrieved_docs)} preprocessed documents from {output_filepath}")
-        count = len(retrieved_docs)
-        idx = retrieved_docs[-1]['question_id'] if len(retrieved_docs) > 0 else 0
-        with tqdm(desc="Preprocessing NaturalQuestions", total=preprocess_length) as pbar:
-            pbar.update(len(retrieved_docs))
-            while len(retrieved_docs) < preprocess_length and idx < len(dataset):
-                question_ids = []
-                questions = []
-                answers = []
-                for _ in range(batch_size):
-                    if idx >= len(dataset):
-                        break
-                    short_answers = dataset[idx]['annotations']['short_answers']
-                    retrieved_doc = {}
-                    answer_list = []
-                    answer_set = set()
-                    for answer in short_answers:
-                        if answer['text'] is None or len(answer['text']) == 0:
-                            continue
-                        for txt in answer['text']:
-                            if txt in answer_set:
-                                continue
-                            answer_set.add(txt)
-                            answer_list.append(txt)
-                    if len(answer_list) == 0:
-                        if preprocess_length == len(dataset):
-                            pbar.update(1)
-                        idx += 1
-                        continue
-                    question_ids.append(idx)
-                    questions.append(dataset[idx]['question']['text'])
-                    answers.append(answer_list)
-                    idx += 1
-                    if len(retrieved_docs) + len(questions) >= preprocess_length:
-                        break
-                batch_contexts = self.searcher.batch_search(questions, question_ids, k=top_k, threads=threads)
-                for question_id, question, answer in zip(question_ids, questions, answers):
-                    retrieved_doc = {}
-                    retrieved_doc['question_id'] = question_id
-                    retrieved_doc['question'] = question
-                    retrieved_doc['answer'] = answer
-                    contexts = batch_contexts[question_id]
-                    context_score_list = []
-                    context_list = []
-                    for context in contexts:
-                        context_list.append(json.loads(self.searcher.doc(context.docid).raw())["contents"])
-                        context_score_list.append(float(context.score))
-                    retrieved_doc['contexts'] = context_list
-                    retrieved_doc['context_scores'] = context_score_list
-                    retrieved_docs.append(retrieved_doc)
-                pbar.update(len(questions))
-                with open(output_filepath, 'w', encoding='utf-8') as f:
-                    json.dump(retrieved_docs, f, ensure_ascii=False, indent=4)
-            pbar.close()
-        return retrieved_docs
+    def retrieve_batch(self, questions: List[str], question_ids: List[int] = None, top_k: int = 5, threads: int = 1) -> Tuple[Dict[str, List], Dict[str, Tuple[List, List]]]:
+        """
+        Retrieve batch of questions
+        Args:
+            questions (List[str]): List of questions to retrieve documents for.
+            question_ids (List[int], optional): List of question IDs corresponding to the questions. Defaults to None. if None, question IDs will be set to range(len(questions)).
+            top_k (int, optional): Number of top documents to retrieve for each question. Defaults to 5.
+            threads (int, optional): Number of threads to use for retrieval. Defaults to 1.
+        Returns: 
+            Dict[str, List] - a dictionary holding the raw search results, with the query ids as keys and the corresponding lists of search results as the values.
+            Dict[str, Tuple[List, List]] - a dictionary holding the corresponding list of retrieved text contexts and list of scores with question id as keys.
+        """
+        if question_ids is None:
+            question_ids = list(map(str, range(len(questions))))
+        batch_contexts = self.store.batch_search(questions, question_ids, top_k=top_k, threads=threads)
+        answers = {}
+        for question, question_id in zip(questions, question_ids):
+            contexts = batch_contexts[question_id]
+            texts = [res["text"] for res in contexts]
+            scores = [res["score"] for res in contexts]
+            answers[question_id] = (texts, scores)
+        return batch_contexts, answers
     
 if __name__ == "__main__":
-    retriever = DPRRetriever()
-    retriever.preprocess_triviaqa(filepath="../data/", top_k=20)
-    retriever.preprocess_nq(filepath="../data/", top_k=20 , split="validation")
+    question = "KHI is the international code for which Asian airport?"
+    retriever = BM25Retriever()
+    answers = retriever.retrieve(question)
+    for answer in answers:
+        print(f"DocID: {answer.docid}, Score: {answer.score}")
+        print(json.loads(answer.lucene_document.get('raw')).get("contents"))
+        print("-"*50)
+    from reranker import BgeReranker
+    reranker = BgeReranker()
+    contexts = [json.loads(answer.lucene_document.get('raw')).get("contents") for answer in answers]
+    ranked_contexts = reranker.rerank(question, contexts)
+    print("Ranked Contexts:")
+    for context in ranked_contexts:
+        print(context)
+        print("-"*50)
