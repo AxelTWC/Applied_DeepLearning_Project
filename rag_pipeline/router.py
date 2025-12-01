934c8a05056fc96d8cb6e8feb23f4c11feeb5a0a
“buruei” <buruei@hotmail.com>
2025-12-01 11:22:55 -0500

    style: change the name from Iterative to be Adaptive

--- rag_pipeline/router.py
+++ rag_pipeline/router.py
@@ -1,5 +1,5 @@
 from .generator import Generator
-from .prompt import ITERATIVE_ROUTER_SYSTEM_PROMPT, ITERATIVE_ROUTER_INITIAL_PROMPT, ITERATIVE_ROUTER_SEQUENTIAL_PROMPT
+from .prompt import ADAPTIVE_ROUTER_SYSTEM_PROMPT, ADAPTIVE_ROUTER_INITIAL_PROMPT, ADAPTIVE_ROUTER_SEQUENTIAL_PROMPT
 from typing import List, Optional
 
 class Router():
@@ -14,19 +14,19 @@ class Router():
         pass
     
     
-class InterativeRouter(Router):
+class AdaptiveRouter(Router):
     
     def __init__(self, generator: Generator):
         self.generator = generator
         self.history = [{
             "role": "system",
-            "content": ITERATIVE_ROUTER_SYSTEM_PROMPT.strip()
+            "content": ADAPTIVE_ROUTER_SYSTEM_PROMPT.strip()
         }]
         
     def route(self, question: str, references: Optional[str] = None) -> List[str]:
-        query = ITERATIVE_ROUTER_INITIAL_PROMPT.format(question=question).strip()
+        query = ADAPTIVE_ROUTER_INITIAL_PROMPT.format(question=question).strip()
         if references:
-            query = ITERATIVE_ROUTER_SEQUENTIAL_PROMPT.format(question=question, References=references).strip()
+            query = ADAPTIVE_ROUTER_SEQUENTIAL_PROMPT.format(question=question, References=references).strip()
         self.history.append({
             "role": "user",
             "content": query
