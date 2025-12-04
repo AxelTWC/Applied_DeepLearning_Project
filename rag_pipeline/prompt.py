adfe7af49983204660ece9324d0f73dfcff96a03
“buruei” <buruei@hotmail.com>
2025-12-04 14:03:35 -0500

    refactor: use <subtopic> tag to label all subtopics

--- rag_pipeline/prompt.py
+++ rag_pipeline/prompt.py
@@ -1,6 +1,6 @@
 
 ADAPTIVE_ROUTER_SYSTEM_PROMPT = """
-You are an intelligent router that can analyze a question and determine the best sub-topics to search for relevant information by breaking down question into sub-topics. Given a question, you should output a list of sub-topics separated by commas that are most relevant to the question. The topics should be concise and directly related to the key concepts in the question. the sub-topics will be used to retrieve relevant documents from a knowledge base to form the final answer for the question effectively. If you think the references from previous retrievals are sufficient to answer the question, respond with "terminate".
+You are an intelligent router that can analyze a question and determine the best sub-topics to search for relevant information by breaking down question into sub-topics. Given a question, you should output a list of sub-topics separated by <subtopic> tags that are most relevant to the question. The topics should be concise and directly related to the key concepts in the question. the sub-topics will be used to retrieve relevant documents from a knowledge base to form the final answer for the question effectively. If you think the references from previous retrievals are sufficient to answer the question, respond with "terminate".
 # Break down Instuctions:
 1. Analyze the question carefully to understand its main components and key concepts.
 2. Identify the most important aspects of the question that require further exploration.
@@ -16,20 +16,27 @@ You are an intelligent router that can analyze a question and determine the best
 4. The sub-topics should cover different facets of the main question to ensure comprehensive retrieval of information.
 5. Avoid overly broad or vague sub-topics.
 6. Ensure that the sub-topics are clearly related to the main question with all necessary context included for accurate retrieval.
-# Example:
-Quesiton: when was the original stephen king it movie made
-sub-topics: Original Stephen King IT movie, release date of original Stephen King IT movie, director of the original Stephen King IT movie
+# Example Output:
+<subtopic>
+subtopic 1
+</subtopic>
+<subtopic>
+subtopic 2
+</subtopic>
+<subtopic>
+subtopic 3
+</subtopic>
 """.strip()
 
 ADAPTIVE_ROUTER_INITIAL_PROMPT = """
 Question: {question}
-Now, list the subquestions needed to answer the main question, separated by commas.
+Now, list the subquestions needed to answer the main question, separated by <subtopic> tags.
 """.strip()
 
 ADAPTIVE_ROUTER_SEQUENTIAL_PROMPT = """
 Original Question: {question}
 {references}
-Based on the previous subquestions and retrieved contexts, now list additional subquestions needed to answer the main question, separated by commas.
+Based on the previous subquestions and retrieved contexts, now list additional subquestions needed to answer the main question, separated by <subtopic> tags.
 """.strip()
 
 ADAPTIVE_GENERATOR_SYSTEM_PROMPT = """
